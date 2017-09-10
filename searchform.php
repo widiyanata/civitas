1e740eecdac454f762334cffcb3053f7f6ea1661
widiyanata <widyanata48@gmail.com>
2017-09-10 20:26:41 +0700

    fixing search form

--- searchform.php
+++ searchform.php
@@ -3,7 +3,6 @@
     <label class="sr-only" for="search">Search in <?php echo home_url( '/' ); ?></label>
     <span class="input-group-addon"><i class="fa fa-search"></i></span>
     <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" class="form-control" placeholder="Search"/>
-    <!-- <input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search.png" /> -->
-    <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+    <!-- <span class="input-group-addon close-search"><i class="fa fa-times"></i></span> -->
   </div>
 </form>
