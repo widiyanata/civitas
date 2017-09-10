92640c21c1c16a71cd36238db2ecff40bb504e77
widiyanata <widyanata48@gmail.com>
2017-09-10 19:58:34 +0700

    minor changes

--- archive.php
+++ archive.php
@@ -18,9 +18,9 @@ get_header(); ?>
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header mt-0 text-uppercase">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_title( '<h3 class="h2 page-title">', '</h3>' );
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
@@ -34,8 +34,7 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-		
-				// get_template_part( 'template-parts/content', get_post_format() );
+
 				get_template_part( 'template-parts/content', 'archive' );
 
 			endwhile;
