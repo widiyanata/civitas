e1d4deccc5d5223036b49e08e679fdb12991cb65
widiyanata <widyanata48@gmail.com>
2017-09-28 17:58:03 +0700

    Remove unnecessary elements

--- 404.php
+++ 404.php
@@ -16,53 +16,28 @@ get_header(); ?>
 
 				<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 columns">
 
-			<section class="error-404 not-found">
-				<header class="page-header mt-0 text-uppercase">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'civitas' ); ?></h1>
-				</header><!-- .page-header -->
+					<section class="error-404 not-found">
+						<header class="page-header mt-0 text-uppercase">
+							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'civitas' ); ?></h1>
+						</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'civitas' ); ?></p>
+						<div class="page-content">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'civitas' ); ?></p>
 
-					<?php
-						get_search_form();
+							<?php
+								get_search_form();
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+							?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'civitas' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+						</div><!-- .page-content -->
+					</section><!-- .error-404 -->
 
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'civitas' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 sidebar-warp columns">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
 		</div>
-		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 sidebar-warp columns">
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
-	</div>
 	</div>
 
 <?php
